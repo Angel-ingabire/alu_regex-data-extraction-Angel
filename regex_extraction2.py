61d6d110d5107c0bff6cf016abb622a4152d71f3
Angel-ingabire <a.ingabire2@alustudent.com>
2025-02-13 04:19:49 -0800

    URL

--- regex_extraction2.py
+++ regex_extraction2.py
@@ -3,4 +3,9 @@ def validate_url(url):
     pattern = r'^(https?://)?(www\.)?[\w.-]+\.[a-z]{2,6}(/\S*)?$'
     return bool(re.match(pattern, url))
 
+def validate_url(url):
+    """Validates URLs (http, https, subdomains, and paths)."""
+    pattern = r'^(https?://)?(www\.)?[\w.-]+\.[a-z]{2,6}(/\S*)?$'
+    return bool(re.fullmatch(pattern, url))
+
 
