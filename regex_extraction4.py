914fbfc13241a1b02a4fb3a494233b220179a950
Angel-ingabire <a.ingabire2@alustudent.com>
2025-02-13 04:34:04 -0800

    phone number

--- regex_extraction4.py
+++ regex_extraction4.py
@@ -1,4 +1,12 @@
 import re
-def validate_credit_card(card):
-    pattern = r'^\d{4}[-\s]?\d{4}[-\s]?\d{4}[-\s]?\d{4}$'
-    return bool(re.match(pattern, card))
+
+def validate_phone_number(phone):
+    """Validates phone numbers (various formats)."""
+    pattern = r'^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$'
+    return bool(re.fullmatch(pattern, phone))
+
+
+phones = ["(123) 456-7890", "123-456-7890", "123.456.7890", "000-000"]
+for phone in phones:
+    print(f"{phone}: {' Valid' if validate_phone_number(phone) else ' Invalid'}")
+
