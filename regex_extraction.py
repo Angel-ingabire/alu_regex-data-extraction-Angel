e4f6854c91740d76830e653bcee90a8a95e6e53c
Angel-ingabire <a.ingabire2@alustudent.com>
2025-02-13 04:12:38 -0800

    email

--- regex_extraction.py
+++ regex_extraction.py
@@ -2,3 +2,8 @@ import re
 def validate_email(email):
     pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return bool(re.match(pattern, email))
+
+def validate_phone_number(phone):
+    """Validates phone numbers (various formats)."""
+    pattern = r'^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$'
+    return bool(re.fullmatch(pattern, phone))
